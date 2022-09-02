02f09db553361bacac70efb9b5475c92f63dd1f6
jey707 <jey707@naver.com>
2022-09-02 20:59:24 +0900

    Server requset, response, middleware

--- src/server.js
+++ src/server.js
@@ -1,7 +1,28 @@
 import express from "express";
+import morgan from "morgan";
+
+const PORT = 4000;
 
 const app = express();
-const PORT = 4000;
+const logger = morgan("dev");
+
+// const gassipMiddleware = (req, res, next) => {
+//   console.log(`Someone is going to:${req.url}`);
+//   next();
+// };
+
+const handleHome = (req, res) => {
+  return res.send("Hello main");
+};
+const handleLogin = (req, res) => {
+  return res.send("Here login");
+};
+
+// app.use(gassipMiddleware);
+
+app.use(logger);
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`Server listening on port ${PORT}`);
 
