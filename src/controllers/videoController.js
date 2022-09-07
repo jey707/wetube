4564ed78b322521da92b0da9f9eccc79fb6eddf6
jey707 <jey707@naver.com>
2022-09-07 17:08:24 +0900

    mongoDb install, DB Connection complete

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -45,7 +45,19 @@ export const postEdit = (req, res) => {
 };
 
 export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-  return res.send("Delete Video");
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,15 +3,14 @@ import {
   watch,
   getEdit,
   postEdit,
-  upload,
-  deleteVideo,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit); //하나의 url에 get,post방식 쓰일떄
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-videoRouter.get("/upload", upload);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
