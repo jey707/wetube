9113280b0d2f5df1eb9254dd3bf56088ca82036f
jey707 <jey707@naver.com>
2022-09-22 18:02:35 +0900

    views Update

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import apiRouter from "./routers/apiRouter";
 import { localsMiddleware } from "./middlewares";
 
 const app = express();
@@ -43,5 +44,6 @@ app.use("/users", userRouter);
 app.use("/uploads", express.static("uploads"));
 app.use("/img", express.static("src/img"));
 app.use("/static", express.static("assets"));
+app.use("/api", apiRouter);
 
 export default app;
