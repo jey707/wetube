b3fcde9a2484042f430ea41090c5679b4ca9520b
jey707 <jey707@naver.com>
2022-09-27 19:57:39 +0900

    isHeroku

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,16 +9,23 @@ const s3 = new aws.S3({
   },
 });
 
-const multerUploader = multerS3({
+const isHeroku = process.env.NODE_ENV === "production";
+
+const s3ImageUploader = multerS3({
   s3: s3,
-  bucket: "wetubebebe",
+  bucket: "wetubebebe/images",
   acl: "public-read",
 });
-
+const s3VideoUploader = multerS3({
+  s3: s3,
+  bucket: "wetubebebe/videos",
+  acl: "public-read",
+});
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Wetube";
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user || {};
+  res, (locals.isHeroku = isHeroku);
   next();
 };
 
@@ -45,12 +52,12 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
-  storage: multerUploader,
+  storage: isHeroku ? s3ImageUploader : undefined,
 });
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: {
     fileSize: 10000000,
   },
-  storage: multerUploader,
+  storage: isHeroku ? s3VideoUploader : undefined,
 });
