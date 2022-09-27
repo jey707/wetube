98270598e5bee095df3bc8a0c0f9f83aff4ee99a
jey707 <jey707@naver.com>
2022-09-27 19:33:45 +0900

    s3 uploads

--- src/server.js
+++ src/server.js
@@ -37,8 +37,11 @@ app.use((req, res, next) => {
 });
 app.use(flash());
 app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 app.use(localsMiddleware);
